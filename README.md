a359ffa7a0a9c8168a3fda8fa83ad5a0ec9006eb
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2023-10-25 10:37:36 -0700

    Include webserver

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Graphic Equalizer for visualizing output from [Soundb0ard](https://github.com/si
 Visualizer Implementation based on [this article](https://orangeable.com/javascript/equalizer-web-audio-api).
 
 AudioWorklet and SharedArrayBuffer implementation based on [this](https://googlechromelabs.github.io/web-audio-samples/audio-worklet/design-pattern/shared-buffer/)
+
+SharedArrayBuffer is disabled by default in browsers [due to potential vulns like Spectre](https://vercel.com/guides/fix-shared-array-buffer-not-defined-nextjs-react). Serve this content in this dir via the included python script which sets the correct heaaders for a localhost connection.
---
+++ webserv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
